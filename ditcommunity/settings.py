7de5148304c3f8e5e100a50e49ffcb12dc4126e9
BeautifulPixelsTech <beautifulpixelstech@gmail.com>
2025-10-18 21:00:07 +0300

    FIX: Update ALLOWED_HOSTS to restrict access to specific hosts

--- ditcommunity/settings.py
+++ ditcommunity/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(f2bg_p_byp-f*ryfu(_lwodmu8qbc#!q5j0l=c&5glyk^fe1)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['pydit.deploy.tz', '127.0.0.1', 'localhost']
 
 
 # Application definition
